b70c79946f92b4460b24eeaa9a08db75e8191b43
Afran-rafi <fahimrrrrr@gmail.com>
2022-04-30 21:41:02 +0600

    delete done

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ async function run() {
             const result = await itemsCollection.insertOne(newItems);
             res.send(result);
         });
+
+        // DELETE
+        app.delete('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally {
 
