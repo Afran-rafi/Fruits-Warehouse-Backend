cf4f2fd89df56fea3ddedac6691a19741b5a05a8
Afran-rafi <fahimrrrrr@gmail.com>
2022-04-30 16:57:58 +0600

    fruits details done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
@@ -17,12 +17,43 @@ async function run() {
         await client.connect()
         const itemsCollection = client.db("Fruits-Warehouse").collection("fruits-items")
 
+        // All fruits items
         app.get('/inventory', async (req, res) => {
             const query = {}
             const cursor = itemsCollection.find(query)
             const items = await cursor.toArray()
             res.send(items)
         })
+
+        // Fruits Details
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        });
+
+        // Update Fruits quantity
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateFruits = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updated = {
+                $set: {
+                    quantity: updateFruits.quantity,
+                }
+            };
+            const result = await itemsCollection.updateOne(filter, updated, options);
+            res.send(result);
+        })
+
+        // POST(Add Fruits)
+        app.post('/inventory', async (req, res) => {
+            const newItems = req.body;
+            const result = await itemsCollection.insertOne(newItems);
+            res.send(result);
+        });
     }
     finally {
 
