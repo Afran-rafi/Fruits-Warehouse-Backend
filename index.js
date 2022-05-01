3d308fcc74746545d8e7dcc7e30c322b7ddbf7aa
Afran-rafi <fahimrrrrr@gmail.com>
2022-05-01 16:06:03 +0600

    myItems fruits done

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function run() {
     try {
         await client.connect()
         const itemsCollection = client.db("Fruits-Warehouse").collection("fruits-items")
+        const myItemsCollection = client.db("Fruits-Warehouse").collection("myItems")
 
         // All fruits items
         app.get('/inventory', async (req, res) => {
@@ -48,13 +49,6 @@ async function run() {
             res.send(result);
         })
 
-        // POST(Add Fruits))
-        app.post('/inventory', async (req, res) => {
-            const newItems = req.body;
-            const result = await itemsCollection.insertOne(newItems);
-            res.send(result);
-        });
-
         // DELETE
         app.delete('/inventory/:id', async (req, res) => {
             const id = req.params.id;
@@ -62,9 +56,23 @@ async function run() {
             const result = await itemsCollection.deleteOne(query);
             res.send(result);
         });
+
+        // POST(Add Fruits))
+        app.post('/myItems', async (req, res) => {
+            const newItems = req.body;
+            const result = await myItemsCollection.insertOne(newItems);
+            res.send(result);
+        });
+
+        // myItems Fruits Collection
+        app.get('/myItems', async (req, res) => {
+            const query = {}
+            const cursor = myItemsCollection.find(query)
+            const myItems = await cursor.toArray()
+            res.send(myItems)
+        })
     }
     finally {
-
     }
 }
 
